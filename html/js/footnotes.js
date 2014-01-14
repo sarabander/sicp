240878fb918be8854ab29dfa34b5f0eed3ea910a
Andres Raba <sarabanderest@gmail.com>
2014-01-14 14:16:44 +0200

    replace focusout with blur, make <body> clickable
    an attempt to work around the peculiarities of touch interface

--- html/js/footnotes.js
+++ html/js/footnotes.js
@@ -8,19 +8,23 @@ $(document).ready(function () {
   var Footnotes = {
     footnotetimeout: false,
     setup: function () {
+      var body = $("body");
       var footnotelinks = $("a.footnote_link");
       footnotelinks.attr('tabindex', '0'); 
       // Don't follow footnote link on click:
       footnotelinks.click(function () { return false; });
 
+      body.unbind('click', Footnotes.bodyclick);
       footnotelinks.unbind('click', Footnotes.footnoteclick);
-      footnotelinks.unbind('focusout', Footnotes.footnoteout);
+      footnotelinks.unbind('blur', Footnotes.footnoteout);
 
+      body.bind('click', Footnotes.bodyclick);
       // Bind new behaviour where click will pop up the footnote:
       footnotelinks.bind('click', Footnotes.footnoteclick);
       // ...and click outside of popup will make it disappear:
-      footnotelinks.bind('focusout', Footnotes.footnoteout);
+      footnotelinks.bind('blur', Footnotes.footnoteout);
     },
+    bodyclick: function () { void(0); },
     footnoteclick: function () {
       clearTimeout(Footnotes.footnotetimeout);
       $(popup).stop();
@@ -31,11 +35,11 @@ $(document).ready(function () {
 
       var div = $(document.createElement('div'));
       div.attr('id', popup.substr(1));
-      // To be able to fire focusout event when clicked outside:
+      // To be able to fire blur event when clicked outside:
       div.attr('tabindex', '0'); 
 
       div.bind('click', Footnotes.divclick);
-      div.bind('focusout', Footnotes.footnoteout);
+      div.bind('blur', Footnotes.footnoteout);
 
       var el = document.getElementById(id);
       div.html($(el).html());
